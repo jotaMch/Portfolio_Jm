4826f7baa49659529d809a9ede74e7c070fa51d0
Jânderson Machado <jandersonmachado090@gmail.com>
2023-07-10 21:29:29 -0300

    add funções de troca de cor e novas cores

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ const textSub = document.querySelector('.subtitle');
 const text = 'Desenvolvedor Front-end';
 const interval = 80;
 
+function showText() {
+    const char = text.split("").reverse();
+    const type = setInterval(() => {
+        if (!char.length) {
+            return clearInterval(type);
+        } 
+        const next = char.pop();
+        textSub.innerHTML += next;
+    }, interval);
+}
+
+showText();
+
+
 function animar() {
     const info = document.querySelector('#info');
     const btn = document.getElementById('btn');
@@ -33,20 +47,70 @@ function imgPerfil() {
     }
 }
 
-function showText() {
-    const char = text.split("").reverse();
-    const type = setInterval(() => {
-        if (!char.length) {
-            return clearInterval(type);
-        }
-        const next = char.pop();
-        textSub.innerHTML += next;
-    }, interval);
-}
-
-showText();
 
 function scrollToSection(sectionId) {
     const section = document.getElementById(sectionId);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+
+
+//btn toggle
+
+
+const checkbox = document.querySelector('input[type="checkbox"]');
+const hello = document.querySelector('.hello');
+const menuList = document.querySelector('.menu-list'); 
+const menuItems = document.querySelectorAll('.menu-list li');
+const paragrafos = document.querySelectorAll('p');
+const cards = document.querySelectorAll('.back-content');
+const elementsToToggle = document.querySelectorAll('header, .flex, #sobre, #skills, #projetos, .ftr, li');
+
+checkbox.addEventListener('change', function() {
+    // Verifica se o checkbox está marcado
+  if (checkbox.checked) { 
+        elementsToToggle.forEach(function(element) {
+        element.classList.add('light');
+        });
+
+        textSub.style.color = '#222';
+        hello.style.color = '#222';
+        menuList.style.backgroundColor = 'rgba(248, 201, 148, 0.97)';
+
+        paragrafos.forEach(function(paragrafo) {
+        paragrafo.style.color = '#000';
+        });
+
+        cards.forEach(function(card) {
+        card.style.backgroundColor = 'rgba(248, 201, 148, 0.97)';
+        });
+
+        menuItems.forEach(function(item) {
+        item.style.color = '#222';
+        });
+        } else {
+        elementsToToggle.forEach(function(element) {
+        element.classList.remove('light');
+        });
+
+        // Retorna à cor original
+
+        textSub.style.color = ''; 
+        hello.style.color = ''; 
+        menuList.style.backgroundColor = ''; 
+
+        paragrafos.forEach(function(paragrafo) {
+        paragrafo.style.color = ''; 
+        });
+
+        cards.forEach(function(card) {
+        card.style.backgroundColor = ''; 
+        });
+
+        menuItems.forEach(function(item) {
+        item.style.color = ''; 
+    });
+    }
+});
+
+
