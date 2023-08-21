1003a7ffe72094cf3f39a07d89301694e71081c8
Jânderson Machado <jandersonmachado090@gmail.com>
2023-08-21 19:42:24 -0300

    add new margin

--- src/components/SectionInicial.jsx
+++ src/components/SectionInicial.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Code from '../assets/code-2.png';/* 
-import CvDocx from "../assets/curriculo_janderson.docx"; */
+import Code from '../assets/code-2.png';
+import CvDocx from "../assets/curriculo_janderson.docx"; 
 
 function SectionInicio() {
     return (
@@ -16,7 +16,7 @@ function SectionInicio() {
                         em realidade.
                     </p>   
                 <div class="button">
-                    <a href="../assets/curriculo_janderson.docx" class="button-wrapper">
+                    <a href={CvDocx} >
                         <div class="text">Download</div>
                         <span class="icon">
                             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"></path></svg>
