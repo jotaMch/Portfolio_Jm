1f1334d49a7d986fd22da1fbf6a8dc663035ec5f
Jânderson Machado <jandersonmachado090@gmail.com>
2023-09-02 19:44:36 -0300

    atualizando icon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,7 +19,7 @@ function About() {
                             <div className="border_bottom"></div>
                                 <p  className='text-sobre onUp'>
                                 Olá! Sou Jânderson Machado, estudo Analise e Desenvolvimento de sistemas na Estácio. 
-                                No momento estou focado em ReactJS minha busca contínua por aprendizado e abordagem curiosa 
+                                No momento estou focado em ReactJS, minha busca contínua por aprendizado e abordagem curiosa 
                                 me motivam a enfrentar desafios e interfaces intuitivas para seus projetos.
                                 Vamos colaborar para o sucesso do seu projeto!
                                 </p>
@@ -28,12 +28,11 @@ function About() {
                                         contate-me
                                     </button>
                                     <button class="button__curriculo">
-                                        <a href={CvDocx} >
-                                            <div class="text">Currículo< BiAlignLeft /></div>
+                                        
                                             <span class="icon">
                                                 <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"></path></svg>
                                             </span>
-                                        </a>
+                                       
                                     </button>
                                 </div>
                         </div>
